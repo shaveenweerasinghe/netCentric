e6b12eba82bf46c0f57be4b23e5cb4a16c1df146
shaveenweerasinghe <shaveenweerasinghe@yahoo.com>
2020-12-02 22:16:32 +0530

    Scroll reveal in main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,32 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*Scroll Section Active Link*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= screenTop + sectionHeight){
+            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+
+        }else{
+            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+/*Scroll Reveal*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
