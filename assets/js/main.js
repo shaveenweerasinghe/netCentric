af473768a22af1c8fba5841794751e1e096b5cf7
shaveenweerasinghe <shaveenweerasinghe@yahoo.com>
2020-12-02 21:55:00 +0530

    Remove mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,12 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle','nav-menu')
+
+/*Remove menu mobile*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
