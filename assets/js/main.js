a032d0a159f0a94c864e9d0cab03dfaefcfbd106
shaveenweerasinghe <shaveenweerasinghe@yahoo.com>
2020-12-07 19:49:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,10 +34,10 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelectorAll'.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
 
         }else{
-            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
